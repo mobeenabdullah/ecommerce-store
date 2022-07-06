a32b3d84e3b41972a97d1d6f8a0e0a6909def358
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-07-07 01:31:58 +0500

    Filters - Colors

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -82,6 +82,44 @@ const Filters = () => {
             </select>
           </div>
           {/* end of companies */}
+          {/* colors */}
+          <div className="form-control">
+            <h5>color</h5>
+            <div className="colors">
+              {colors.map((c, index) => {
+                if (c === "all") {
+                  return (
+                    <button
+                      key={index}
+                      name="color"
+                      onClick={updateFilters}
+                      data-color="all"
+                      className={`${
+                        color === "all" ? "all-btn active" : "all-btn"
+                      }`}
+                    >
+                      all
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={index}
+                    name="color"
+                    className={`${
+                      color === c ? "color-btn active" : "color-btn"
+                    }`}
+                    style={{ background: c }}
+                    data-color={c}
+                    onClick={updateFilters}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* end of colors */}
         </form>
       </div>
     </Wrapper>
