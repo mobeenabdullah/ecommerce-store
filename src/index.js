2397d930642ed2d7b9fc272e323c8f1bf9883168
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-07-08 06:24:10 +0500

    Auth0 - Provider

--- src/index.js
+++ src/index.js
@@ -9,12 +9,19 @@ import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-m-57qtwv.us.auth0.com"
+    clientId="bKBBnfKBzwIZV0X3ktERooFMWG7xSyBH"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
