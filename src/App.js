1a8935a702e66b5486b32ce1e3b1f45bbb6d99fe
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-07-13 15:53:58 +0500

    Refactor to React Router 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
 import {
@@ -20,27 +20,22 @@ function App() {
       <Router>
         <Navbar />
         <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/cart">
-            <Cart />
-          </Route>
-          <Route exact path="/products">
-            <Products />
-          </Route>
-          <Route exact path="/products/:id" children={<SingleProduct />} />
-          <PrivateRoute exact path="/checkout">
-            <Checkout />
-          </PrivateRoute>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="cart" element={<Cart />}></Route>
+          <Route path="products" element={<Products />}></Route>
+          <Route path="products/:id" element={<SingleProduct />}></Route>
+          <Route
+            path="checkout"
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
         <Footer />
       </Router>
     </AuthWrapper>
